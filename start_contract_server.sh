860f72e7c526661f2449a8a6d295337544c477b0
Matthew Roberts <matthew@roberts.pm>
2015-08-06 14:34:25 +1000

    Added start server scripts.

---
+++ start_contract_server.sh
@@ -0,0 +1,1 @@
+python3.3 -m "coinbend.contract_server" -externalexchange 0 -erateinit BTC_USD_358.03,USD_AUD_1.2116072,LTC_BT0.096000,DOGE_BTC_0.00000056 -skipnet 1 -skipdht 1
--- start_rendezvous_server.sh
+++ start_rendezvous_server.sh
@@ -1,1 +1,1 @@
-python3.3 -m "coinbend.rendezvous_server"
+python3.3 -m "coinbend.rendezvous_server" -externalexchange 0 -erateinit BTC_USD_358.03,USD_AUD_1.2116072,LTC_BT0.096000,DOGE_BTC_0.00000056 -skipnet 1 -skipdht 1
